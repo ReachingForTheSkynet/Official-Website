9c9e0cbedee8129435c72989d123928147807dd0
Paromita Datta <paromitadatta@shreya.lan>
2019-12-17 22:23:15 -0600

    Updated Mission Statement

--- index.js
+++ index.js
@@ -37,3 +37,79 @@ document.getElementById("logo").addEventListener("click", () => {
 window.onbeforeunload = () => {
     window.scrollTo(0, 0);
 }
+let w = 0;
+const go = (setInterval(() => {
+    // console.log("Hello?");
+    // window.requestAnimationFrame(ani);
+    // console.log("Hi?");
+    let ele = document.getElementById("miss");
+    const words = ["Enrich Keystone's Robotics Program", "Create an Engineering Culture for Keystone School", "Create STEM Opportunities", "Grow and Learn Throughout the Year"];
+    ele.innerHTML = words[w];
+    w = (w+1)%words.length;
+}, 2000));
+// let w = 0, op = 0;
+// const ani = () => {
+//     // console.log("Hello?");
+//     window.requestAnimationFrame(ani);
+//     // console.log("Hi?");
+//     let ele = document.getElementById("miss");
+//     const words = ["Enrich Keystone's Robotics Program", "Create an Engineering Culture for Keystone School", "Create STEM Opportunities", "Grow and Learn Throughout the Year"];
+//     ele.innerHTML = words[w];
+//     console.log(ele.style.opacity);
+//     console.log("HI");
+//     if (ele.style.opacity >= 1) {
+//         while (!ele.style.opacity < 0) {
+//             setTimeout(
+//                 () => {
+//                     ele.style.opacity -= 0.1;
+//                     if (ele.style.opacity < 0) {
+//                         ele.style.opacity = 0;
+//                         return;
+//                     }
+//                     console.log("NUM1")
+//                 }, 10
+//             )
+//         }
+//     }
+//     if (ele.style.opacity <= 0) {
+//         while (!ele.style.opacity >= 1) {
+//             setTimeout(
+//                 () => {
+//                     ele.style.opacity += 0.1;
+//                     if (ele.style.opacity >= 1) {
+//                         ele.style.opacity = 1;
+//                         return;
+//                     }
+//                     console.log("NUM2")
+//                 }, 10
+//             )
+//         }
+//         // let y = setInterval(() => {
+//         //     ele.style.opacity += 0.1;
+//         //     if (ele.style.opacity > 1) {
+//         //         ele.style.opacity = 1;
+//         //         clearInterval(y);
+//         //     }
+//         //     console.log("NUM2")
+//         // }, 10)
+//     }
+//     console.log(ele.style.opacity);
+//     // let timer1 = setInterval(() => {
+//     //     if (op >= 1) {
+//     //         clearInterval(timer1);
+//     //     }
+//     //     x.style.opacity = op;
+//     //     // x.style.filter = "alpha(opacity=" + op * 100 + ")";
+//     //     op += op * 0.1;
+//     // }, 10);
+//     // const timer2 = setInterval(function () {
+//     //     if (op <= 0.1) {
+//     //         clearInterval(timer2);
+//     //         w=(w+1)%words.length;
+//     //     }
+//     //     x.style.opacity = op;
+//     //     // x.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//     //     op -= op * 0.1;
+//     // }, 50);
+// };
+// ani();
