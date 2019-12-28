13d2f9f9ec14ba385df3a0acd122a02f5846c235
Paromita Datta <paromitadatta@shreya.lan>
2019-12-28 15:58:39 -0600

    Reverted Changes

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ if (window.innerWidth < window.innerHeight) {
     let menu_clicked = false;
     document.getElementById("menu_control").addEventListener("click", () => {
         if (menu_clicked) {
-            document.getElementById("menu_control").src = "backgrounds/1146025.png";
+            // document.getElementById("menu_control").src = "backgrounds/1146025.png";
             let mobnav = document.getElementById("mobile_navigator");
             mobnav.style.display = "none";
             menu_clicked = !menu_clicked;
         } else {
-            document.getElementById("menu_control").src = "backgrounds/x.png";
+            // document.getElementById("menu_control").src = "backgrounds/x.png";
             let mobnav = document.getElementById("mobile_navigator");
             mobnav.style.display = "block";
             let buttons = document.getElementsByClassName("mob_button");
@@ -48,6 +48,7 @@ if (window.innerWidth < window.innerHeight) {
                     closePanels();
                     if (i >= 6) document.getElementById(panelsToHide[i - 6]).style.display = "block";
                     else document.getElementById(panelsToHide[i]).style.display = "block";
+                    // document.getElementById("menu_control").src = "backgrounds/1146025.png";
                 })
             }
             menu_clicked = !menu_clicked;
